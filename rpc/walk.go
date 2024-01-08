82f7743a0af6574035869ea2664c55f81956c463
Alan Braithwaite <alan@runreveal.com>
2024-01-07 18:10:16 -0800

    hella

---
+++ rpc/walk.go
@@ -0,0 +1,37 @@
+package rpc
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gorilla/mux"
+)
+
+func Walk(r *mux.Router) {
+	err := r.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		pathTemplate, err := route.GetPathTemplate()
+		if err != nil {
+			fmt.Println("Path template err:", err)
+		}
+		pathRegexp, err := route.GetPathRegexp()
+		if err != nil {
+			fmt.Println("Path regexp err:", err)
+		}
+		methods, err := route.GetMethods()
+		if err != nil {
+			if strings.Contains(err.Error(), "doesn't have methods") {
+				methods = []string{"ANY"}
+			} else {
+				fmt.Println("Methods err:", err)
+			}
+		}
+		handler := route.GetHandler()
+		for _, method := range methods {
+			fmt.Printf("%s %s %s %s\n", method, pathTemplate, pathRegexp, handler)
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
