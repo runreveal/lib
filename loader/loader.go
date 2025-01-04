ee8768ec4ce5d0a040380d312c1e031ae876cc7b
Alan Braithwaite <alan@runreveal.com>
2025-01-04 11:52:42 -0800

    add options to loader

--- loader/loader.go
+++ loader/loader.go
@@ -1,6 +1,7 @@
 package loader
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"reflect"
@@ -32,17 +33,17 @@ func LoadConfig(bts []byte, cfg any) error {
 	return nil
 }
 
-type Builder[T any] interface {
-	Configure() (T, error)
+type Builder[O, T any] interface {
+	Configure(...func(O)) (T, error)
 }
 
-type Registry[T any] struct {
-	m map[string]func() Builder[T]
+type Registry[O, T any] struct {
+	m map[string]func() Builder[O, T]
 	sync.RWMutex
 }
 
 var registry = struct {
-	// map[typeString]registry[T] where T is variadic and typeString is:
+	// map[typeString]registry[O, T] where T is variadic and typeString is:
 	// reflect.TypeOf(T).String()
 	m map[string]any
 	sync.RWMutex
@@ -63,7 +64,7 @@ func loadTypeReg(typ string) (any, error) {
 // Register registers a factory method for a type T with the given type name. T
 // is typically an interface that is implmented by the struct of type given by
 // the name.
-func Register[T any](name string, factory func() Builder[T]) {
+func Register[O, T any](name string, factory func() Builder[O, T]) {
 	registry.Lock()
 	defer registry.Unlock()
 
@@ -73,27 +74,28 @@ func Register[T any](name string, factory func() Builder[T]) {
 	typStr := reflect.TypeOf(typ).String()
 	typReg, ok := registry.m[typStr]
 	if !ok {
-		typReg = &Registry[T]{
-			m: make(map[string]func() Builder[T]),
+		typReg = &Registry[O, T]{
+			m: make(map[string]func() Builder[O, T]),
 		}
 		registry.m[typStr] = typReg
 	}
-	registryForType := typReg.(*Registry[T])
+	registryForType := typReg.(*Registry[O, T])
 	registryForType.m[name] = factory
 }
 
-type Loader[T any] struct {
-	Builder[T]
+// Loader is a struct which can dyanmically unmarshal any type T
+type Loader[O, T any] struct {
+	Builder[O, T]
 }
 
-func (b *Loader[T]) UnmarshalJSON(raw []byte) error {
+func (b *Loader[O, T]) UnmarshalJSON(raw []byte) error {
 	typ := new(T)
 	typStr := reflect.TypeOf(typ).String()
 	typReg, err := loadTypeReg(typStr)
 	if err != nil {
 		return err
 	}
-	registryForType := typReg.(*Registry[T])
+	registryForType := typReg.(*Registry[O, T])
 
 	loadType := gjson.Get(string(raw), "type")
 	if !loadType.Exists() {
@@ -110,8 +112,12 @@ func (b *Loader[T]) UnmarshalJSON(raw []byte) error {
 	return json.Unmarshal(raw, b.Builder)
 }
 
-func (l Loader[T]) Configure() (T, error) {
-	return l.Builder.Configure()
+func (l Loader[O, T]) Configure(opts ...func(O)) (T, error) {
+	var t T
+	if l.Builder == nil {
+		return t, errors.New("no type registered for configuration")
+	}
+	return l.Builder.Configure(opts...)
 }
 
 func replaceEnv(v reflect.Value) {
